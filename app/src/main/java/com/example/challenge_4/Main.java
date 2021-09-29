172a3651aa60546150d2b3f5b599fe473d9ddc28
s2356546 <f.gonzalez-novolopez@student.utwente.nl>
2021-09-29 09:15:06 +0200

    Empty project

---
+++ app/src/main/java/com/example/challenge_4/Main.java
@@ -0,0 +1,22 @@
+package com.example.challenge_4;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class Main extends FragmentActivity  {
+
+    private static final String TAG = "Main";
+
+    @Override
+    public final void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Call the class/method that initializes the app and asks for permissions
+
+        // Get the bluetooth devices that are connected
+    }
+
+
+
+}
